b38b8c8bf3c0c9a9cfc0299e2276278b18ddf945
Alexander Komarov <ak@akxcv.com>
2018-02-20 23:49:20 +0300

    Initial Commit

---
+++ iage/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iage
+//
+//  Created by Александр Комаров on 20.02.2018.
+//  Copyright © 2018 Александр Комаров. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
